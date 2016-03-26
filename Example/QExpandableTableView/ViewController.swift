844b123ac01bc9b0af3c7b97e4648cef9ded5fc2
Vincent Le <vinnyoodles@gmail.com>
2016-03-26 11:49:56 -0400

    remove methods from user side

--- Example/QExpandableTableView/ViewController.swift
+++ Example/QExpandableTableView/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import QExpandableTableView
 
-class ViewController: UIViewController
+class ViewController: QExpandableTableView
 {
     @IBOutlet weak var ExTableView: UITableView!
 
@@ -18,60 +18,22 @@ class ViewController: UIViewController
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override public func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        super.configureTableView(ExTableView)
+        super.loadCellDescriptors(ExTableView)
+        
+        //print(cellDescriptors)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    /**
-     *  This will load all cell attributes from the p-list. You will have to make the p-list yourself.
-     *  Once you make the p-list you can specifiy what rows you can put it and then this method will allow you
-     to load those contents into an array called cellDescriptors that you will use throughout the project.
-     */
-    public func loadCellDescriptors()
-    {
-        if let path = NSBundle.mainBundle().pathForResource("CellDescriptor", ofType: "plist")
-        {
-            cellDescriptors = NSMutableArray(contentsOfFile: path)
-            getIndicesOfVisibleRows()
-            ExTableView.reloadData();
-            
-            
-            // your own tableview .reloadData()
-            
-            // properties that we need for this cocoa-pod
-            // isExpandable = bool telling if the view can expand or not
-            // isExpanded = if the cell is expanded or collapsed
-            // isVisible = if the cell is visible or not
-            // primaryTitle = title for the main cell
-            // secondaryTitle = title fo the sub cells under the main cell
-            // cellIdentifier = identifier of the custom cell that we made in the project
-            // additionalRows = number of extra rows we need after the primary Cell
-        }
-    }
-    
-    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
-    {
-        let indexOfTappedRow = visibleRowsPerSection[indexPath.section][indexPath.row]
-        
-        if cellDescriptors[indexPath.section][indexOfTappedRow]["isExpandable"] as! Bool == true
-        {
-            var shouldExpandAndShowSubRows = false
-            if cellDescriptors[indexPath.section][indexOfTappedRow]["isExpanded"] as! Bool == false {
-                // In this case the cell should expand.
-                shouldExpandAndShowSubRows = true
-            }
-            
-            for i in (indexOfTappedRow + 1)...(indexOfTappedRow + (cellDescriptors[indexPath.section][indexOfTappedRow]["additionalRows"] as! Int))
-            {
-                cellDescriptors[indexPath.section][i].setValue(shouldExpandAndShowSubRows, forKey: "isVisible")
-            }
-            
-        }
-        getIndicesOfVisibleRows()
-        ExTableView.reloadSections(NSIndexSet(index: indexPath.section), withRowAnimation: UITableViewRowAnimation.Fade)
-    }
 
 }
 
--- Pod/Classes/QExpandableTableView.swift
+++ Pod/Classes/QExpandableTableView.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import UIKIt
 
 // This cocoa-pod will be for used as an Expandable Table View. In this cocoa-pod you
 // will be able to expand and collaspe rows according to what you need. 
@@ -17,41 +16,33 @@ var cellDescriptors: NSMutableArray!
 /// This is a double array because you have section and all the rows. In this example there is only 1 section
 var visibleRowsInSection = [[Int]]()
 
-class QExpandableTableView: UIViewController, UITableViewDelegate, UITableViewDataSource
+public class QExpandableTableView: UIViewController, UITableViewDelegate, UITableViewDataSource
+    
 {
-    override func viewDidLoad()
+    override public func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     
-    override func viewWillAppear(animated: Bool) {
+    override public func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
-        configureTableView()
-        loadCellDescriptors()
-        
-        //print(cellDescriptors)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     // This is for Custom cells but this is a basic example, so we will only expand basic cells.
     // The normal cell is just the usual cell you will see, and when you exapnd the basic cell, you will 
     // see the valuePickerCell, which contains the specific value you want to see.
     
-    func configureTableView()
+    public func configureTableView(tableView: UITableView)
     {
-        tblExpandable.delegate = self
-        tblExpandable.dataSource = self
-        tblExpandable.tableFooterView = UIView(frame: CGRectZero)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView(frame: CGRectZero)
         
-        tblExpandable.registerNib(UINib(nibName: "NormalCell", bundle: nil), forCellReuseIdentifier: "idCellNormal")
-        tblExpandable.registerNib(UINib(nibName: "ValuePickerCell", bundle: nil), forCellReuseIdentifier: "idCellValuePicker")
+        tableView.registerNib(UINib(nibName: "NormalCell", bundle: nil), forCellReuseIdentifier: "idCellNormal")
+        tableView.registerNib(UINib(nibName: "ValuePickerCell", bundle: nil), forCellReuseIdentifier: "idCellValuePicker")
         
     }
     
@@ -60,23 +51,14 @@ class QExpandableTableView: UIViewController, UITableViewDelegate, UITableViewDa
     *  Once you make the p-list you can specifiy what rows you can put it and then this method will allow you
        to load those contents into an array called cellDescriptors that you will use throughout the project.
     */
-    func loadCellDescriptors()
+    public func loadCellDescriptors(tableView: UITableView)
     {
         if let path = NSBundle.mainBundle().pathForResource("CellDescriptor", ofType: "plist")
         {
             cellDescriptors = NSMutableArray(contentsOfFile: path)
             getIndicesOfVisibleRows()
-            
-            // your own tableview .reloadData()
-            
-            // properties that we need for this cocoa-pod
-            // isExpandable = bool telling if the view can expand or not
-            // isExpanded = if the cell is expanded or collapsed
-            // isVisible = if the cell is visible or not
-            // primaryTitle = title for the main cell
-            // secondaryTitle = title fo the sub cells under the main cell
-            // cellIdentifier = identifier of the custom cell that we made in the project
-            // additionalRows = number of extra rows we need after the primary Cell
+            print(visibleRowsInSection)
+            tableView.reloadData()
         }
     }
     
@@ -89,14 +71,15 @@ class QExpandableTableView: UIViewController, UITableViewDelegate, UITableViewDa
         
         for currentSectionCells in cellDescriptors {
             var visibleRows = [Int]()
+//            print((currentSectionCells as! [[String: AnyObject]]).count - 1)
             
-            for row in 0...((currentSectionCells as! [[String: AnyObject]]).count - 1) {
+            for row in 0...( ( currentSectionCells as! [[String: AnyObject]] ).count - 1) {
                 if currentSectionCells[row]["isVisible"] as! Bool == true {
                     visibleRows.append(row)
                 }
             }
             
-            visibleRowsPerSection.append(visibleRows)
+            visibleRowsInSection.append(visibleRows)
         }
         
         
@@ -111,56 +94,14 @@ class QExpandableTableView: UIViewController, UITableViewDelegate, UITableViewDa
      */
     func getCellDescriptorForIndexPath(indexPath: NSIndexPath) -> [String: AnyObject]
     {
-        let indexOfVisibleRow = visibleRowsInSection[indexPath.section][indexPath.row]
-        let cellDescriptor = cellDescriptors[indexPath.section][indexPath.row] as! [String: AnyObject]
-        return cellDescriptor
+        print(indexPath.row)
+        return cellDescriptors[0][indexPath.row] as! [String: AnyObject]
     }
     
-    /**
-     Returns the number of Sections in the Table View
-     
-     - parameter tableView: This the tableview that you have in the app.
-     
-     - returns: Returns the number of sections you have in the tableview
-     */
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int
-    {
-        if cellDescriptors != nil
-        {
-            return cellDescriptors.count
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    
-    /**
-     This method will set the height for the row of the specific cells. You can add heights to your own unique cells
-     
-     - parameter tableView: This is the tableview that you use in the app
-     - parameter indexPath: This keeps track of where you are in the tableView
-     
-     - returns: Returns a float for the specific cell height
-     */
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath)
-        
-        switch currentCellDescriptor["cellIdentifier"] as! String
-        {
-            case "idCellNormal":
-                return 60.0
-            
-            default:
-                return 44.0
-        }
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-    {
-        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath)
-        let cell = tableView.dequeueReusableCellWithIdentifier(currentCellDescriptor["cellIdentifier"] as! String, forIndexPath: indexPath) as! CustomCell
+        let cell = tableView.dequeueReusableCellWithIdentifier(currentCellDescriptor["cellIdentifier"] as! String, forIndexPath: indexPath) as! UITableViewCell
         
         if currentCellDescriptor["cellIdentifier"] as! String == "idCellNormal"
         {
@@ -181,6 +122,7 @@ class QExpandableTableView: UIViewController, UITableViewDelegate, UITableViewDa
        
         
         return cell
+        return UITableViewCell()
     }
 
     
@@ -192,11 +134,17 @@ class QExpandableTableView: UIViewController, UITableViewDelegate, UITableViewDa
      
      - returns: Returns the number of rows in the desired section of the desired tableview
      */
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return visibleRowsInSection[section].count
+        print(visibleRowsInSection)
+        if visibleRowsInSection != [] {
+            return visibleRowsInSection[0].count
+        }
+        else {
+            return 0
+        }
     }
     
     
-
+    
 }
